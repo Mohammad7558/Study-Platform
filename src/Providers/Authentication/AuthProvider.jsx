8f42104d27af81dcc149dca1ef9947cb8473633e
Mohammad <108111892+Mohammad7558@users.noreply.github.com>
2025-07-09 00:30:17 +0600

    boilerplat-added

---
+++ src/Providers/Authentication/AuthProvider.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { AuthContext } from './AuthContext';
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    
+
+    const userInfo = {
+        user,
+        loading,
+        setLoading,
+        setUser
+    }
+
+    return <AuthContext.Provider value={userInfo}>{children}</AuthContext.Provider>
+};
+
+export default AuthProvider;
