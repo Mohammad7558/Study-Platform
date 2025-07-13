f190c03acf26d9dbbc3c4d921641d4f9b9c4c4b1
Mohammad <108111892+Mohammad7558@users.noreply.github.com>
2025-07-14 05:06:06 +0600

    added-some-extra

--- src/Pages/AdminPages/SingleUser.jsx
+++ src/Pages/AdminPages/SingleUser.jsx
@@ -49,9 +49,9 @@ const SingleUser = ({ user, currentUserEmail, onRoleUpdate, formattedCreatedAt }
             <td className="px-6 py-4">
                 <div className="flex items-center">
                     <div className="flex-shrink-0 h-10 w-10 rounded-full overflow-hidden bg-gray-100">
-                        {user.photoURL ? (
+                        {user.photoUrl ? (
                             <img 
-                                src={user.photoURL} 
+                                src={user.photoUrl} 
                                 alt={user.name} 
                                 className="h-full w-full object-cover"
                                 onError={(e) => {
